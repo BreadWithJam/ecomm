6491c54c84ef1a36acadc11459325417d89ff398
BreadWithJam <lubayjamyca@gmail.com>
2025-09-22 17:21:01 +0800

    1st

---
+++ ecommerce/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import landingpage from './components/landingpage.vue'
+</script>
+
+<template>
+  <div>
+  </div>
+  <landingpage msg="Vite + Vue" />
+</template>
+
+<style scoped>
+
+</style>
---
+++ ecommerce/src/components/landingpage.vue
@@ -0,0 +1,37 @@
+<script>
+
+
+</script> 
+
+
+
+<template>
+    <body class = "bg">
+        
+    
+<div class = "container">
+    <div class = "row align-items-center">
+        <div class = "col-lg-6 d-flex align-items-center"style = "height: 50px">
+           
+            <img src="C:\src\ecomm\ecommerce\src\assets\images\coffee.jpg" alt="Logo" style="width: 40px; height: 40px; margin-right: 15px;">
+                <input type="text" name="" id="" placeholder="Search" class="form-control form-control-lg rounded-pill shadow-sm"
+                style=" width: 650px; height: 15px; margin-left: 60px;">
+            
+        </div> 
+        <div class="col-12">
+            <div>
+                2nd column
+            </div>
+        </div>
+    </div>
+   
+</div>
+</body>
+</template>
+
+
+
+<style>
+
+
+</style>
