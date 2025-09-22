5a5d9cccdedd752366e917cc6dc8b88c69b7d520
BreadWithJam <lubayjamyca@gmail.com>
2025-09-22 19:36:56 +0800

    landing skel

--- ecommerce/src/components/landingpage.vue
+++ ecommerce/src/components/landingpage.vue
@@ -7,31 +7,63 @@
 
 <template>
     <body class = "bg">
-        
-    
 <div class = "container">
     <div class = "row align-items-center">
-        <div class = "col-lg-6 d-flex align-items-center"style = "height: 50px">
+        <div class = "col-lg-12 d-flex align-items-center"style = "height: 50px">
            
-            <img src="C:\src\ecomm\ecommerce\src\assets\images\coffee.jpg" alt="Logo" style="width: 40px; height: 40px; margin-right: 15px;">
-                <input type="text" name="" id="" placeholder="Search" class="form-control form-control-lg rounded-pill shadow-sm"
-                style=" width: 650px; height: 15px; margin-left: 60px;">
+            <img src="C:\src\ecomm\ecommerce\src\assets\images\coffee.jpg" alt="Logo" style="width: 40px; height: 45px; margin-right: 15px;">
+                <input type="text" name="" id="" placeholder="Search" class="form-control rounded-pill shadow-sm"
+                style=" width: 550px; height: 40px; margin-left: 60px;">
+               <button class = "btn searchbtn" style="margin-left: 17px; height: 65%; width: 5%; border-radius: 12px;"> 
+                <i class="bi bi-search"> </i>
+               </button>
+
+               <div style="margin-left: 15%; margin-right: 0;">
+                <button class="ls-btn"> Login </button>
+               <button class="ls-btn" style="margin-left: 10px;">Sign Up</button>
+               </div>
             
         </div> 
+</div>
+ </div>
+        <div class="container-fluid bcontainer mt-4">
+            <div class="row">
         <div class="col-12">
             <div>
-                2nd column
+                  
             </div>
+       
         </div>
     </div>
-   
-</div>
+    </div>
+
 </body>
 </template>
 
 
 
 <style>
+ .searchbtn{
+    background-color: #4CAF50 !important; 
+    color: #ffffff !important;
+ }
 
+ .ls-btn{
+    background-color: transparent;
+    color: #333333;
+    border-radius: 8px;
+ }
+ 
+ .ls-btn:hover{
+    border-color: #4CAF50;
+ }
 
+.bcontainer{
+    
+    background-color: #FFFFFF;
+    width: 100%;
+    min-height: 100vh;
+    border-radius: 20px;
+    padding: 20px;
+}
 </style>
