27a5a96579a182433d0b666d18a34025a0b7df41
BreadWithJam <lubayjamyca@gmail.com>
2025-09-24 21:05:22 +0800

    dfd

--- ecommerce/src/components/landingpage.vue
+++ ecommerce/src/components/landingpage.vue
@@ -17,7 +17,6 @@
                <button class = "btn searchbtn" style="margin-left: 30px; height: 65%; width: 5%; border-radius: 12px;"> 
                 <i class="bi bi-search"> </i>
                </button>
-
                <div style="margin-left: 17%; margin-right: 0;">
                 <button class="ls-btn"> Login </button>
                <button class="ls-btn" style="margin-left: 10px;">Sign Up</button>
@@ -31,25 +30,25 @@
         <div class="col-7">
             <div id = "carouselContent" class = "carousel slide" data-bs-ride = "carousel">
                 <div class="carousel-inner">
-                    <div class="carousel-item active" data-bs-interval = "1100">
+                    <div class="carousel-item active" data-bs-interval = "1500">
                         <img src="C:\src\ecomm\ecommerce\src\assets\images\bags.png" alt="bags" style = "height: 200px; width: 350px; margin-top: 2%;">
                         <div class="carousel-caption" style="text-align: right;"></div>
                     </div>
                 </div>
                   
-                <div class = "carousel-item" data-bs-interval="1500">
+                <div class = "carousel-item" data-bs-interval="1100">
                         <img src="C:\src\ecomm\ecommerce\src\assets\images\sandals.png" alt="sandals" style = "height: 200px; width: 350px; margin-top: 2%;">
                         <div class="carousel-caption" style="text-align: right;"></div>
                 </div>
             </div>
        
-            <div class="carousel-item" data-bs-interval="1500">
+            <div class="carousel-item" data-bs-interval="1100">
                 <img src="C:\src\ecomm\ecommerce\src\assets\images\womensWear.png" alt="Women's wear'" style = "height: 200px; width: 350px; margin-top: 2%;">
                     <div class="carousel-caption" style="text-align: right">
                     </div>
                      </div>
 
-            <div class="carousel-item" data-bs-interval="1500">
+            <div class="carousel-item" data-bs-interval="1100">
                 <img src="C:\src\ecomm\ecommerce\src\assets\images\Shoes.png" alt="Shoes" style = "height: 200px; width: 350px; margin-top: 2%;">
             </div>
             
